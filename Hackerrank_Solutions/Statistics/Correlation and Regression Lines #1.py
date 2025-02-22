5c310576e9a6410b1d52a7ac54d512d06f866bda
NetsmartzProject <vickykr1703@gmail.com>
2025-02-22 23:47:34 +0530

    Make changes in Coefficient

--- Hackerrank_Solutions/Statistics/Correlation and Regression Lines #1.py
+++ Hackerrank_Solutions/Statistics/Correlation and Regression Lines #1.py
@@ -1,13 +1,19 @@
 import math
 
 temp = -(3/4) 
-flag=-(3/4)
+flag=-(4/3)
 
-#  Temp and Flag are the Coefficients of the equation 3x+4y+8=0 and 4x+3y+7=0
-# Coefficients will tell how corelated the two lines are.
-# We are using Pearson Correlation Coefficient to find the correlation between the two lines.
+# #  Temp and Flag are the Coefficients of the equation 3x+4y+8=0 and 4x+3y+7=0
+# # Coefficients will tell how corelated the two lines are.
+# # We are using Pearson Correlation Coefficient to find the correlation between the two lines.
 
 
-r = math.sqrt(temp*flag)
+r = math.sqrt(temp**2+flag**2)
+r1=-temp%r
+print(round(-r1,2))
 
-print(round(-r,2))
+#  - m1/root(m1^2+m2^2)
+
+
+
+#  
