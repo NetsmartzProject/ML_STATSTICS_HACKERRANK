7c46dd9e635ea15ca9f8dc59b51c1338caa39588
NetsmartzProject <vickykr1703@gmail.com>
2025-02-25 22:26:42 +0530

    Employee table and sorting them in ascending order

---
+++ SQL/Employee Names.py
@@ -0,0 +1,8 @@
+# 1️⃣ SELECT name → Retrieves only the name column from the table.
+# 2️⃣ FROM Employee → Specifies that the data should come from the Employee table.
+# 3️⃣ ORDER BY name → Sorts the retrieved names in ascending order (A to Z).
+
+
+# SELECT name from Employee ORDER BY name;
+
+# This is The Query for retrieving the names from the Employee table and sorting them in ascending order.
