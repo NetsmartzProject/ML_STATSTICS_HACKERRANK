fc354106d506a43ef6079f4c3ddedda273876e62
NetsmartzProject <vickykr1703@gmail.com>
2025-02-25 22:18:32 +0530

    Solved SQL Problem for getting the distinct city

---
+++ SQL/Weather Observation Station 4.py
@@ -0,0 +1,9 @@
+
+# This Query will execute For getting the count of the cities which are repeating in the table.
+
+# Let Suppose "CITY" have  "Bihar","Bihar","UP"
+
+# this query will return 1 as Bihar is repeating in the table.
+
+# SELECT COUNT(CITY)-COUNT(DISTINCT CITY) FROM STATION;
+
