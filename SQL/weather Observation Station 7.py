db751c5c207c9876b71afe14cf7c292986f499f5
NetsmartzProject <vickykr1703@gmail.com>
2025-02-27 23:29:14 +0530

    Solved the Equations list of CITY names ending with vowels

---
+++ SQL/weather Observation Station 7.py
@@ -0,0 +1,14 @@
+
+# Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+# Your result cannot contain duplicates.
+
+
+
+
+
+
+
+
+#  SELECT DISTINCT city 
+#  FROM Station 
+#  WHERE RIGHT(city, 1) IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
